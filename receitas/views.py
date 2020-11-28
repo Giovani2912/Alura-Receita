468a26026fe9cedb279ef8de27f65d7b709f313b
Giovani Foltran <giovanicanovaf@gmail.com>
2020-11-28 09:17:19 -0300

    include e partials

--- receitas/views.py
+++ receitas/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html');
+
+
+def receita(request):
+    return render(request, 'receita.html');
