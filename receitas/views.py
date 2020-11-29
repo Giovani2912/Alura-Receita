e9e5575aaf085853ad59b107c6c6bcd0e84fe1db
Giovani Foltran <giovanicanovaf@gmail.com>
2020-11-28 21:59:21 -0300

    fim do cap 4

--- receitas/views.py
+++ receitas/views.py
@@ -3,8 +3,19 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 # Create your views here.
 
+receitas = {
+    1:'Parmegiana',
+    2:'Escondidinho',
+    3:'Tapioca',
+    4:'Omelete'
+}
+
+dados = {
+    'nome_das_receitas': receitas
+}
+
 def index(request):
-    return render(request, 'index.html');
+    return render(request, 'index.html', dados);
 
 
 def receita(request):
